da96e0621b9cda834d12389a6d857cf375702996
Yuting Hu <yuting.hu.careers@gmail.com>
2024-04-16 14:32:00 -0700

    completed function for add users in the db

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,26 @@
-import bcrypt from 'bcrypt'
+import bcrypt, { hash } from 'bcrypt'
+import prisma from '../lib/prisma.js';
 
-export const register = async(req, res) => {
+export const register = async (req, res) => {
 
     const { username, email, password } = req.body
 
     //HASH the Password
-    const hasedPassword = await bcrypt.hash(password,10)
+    const hasedPassword = await bcrypt.hash(password, 10)
     console.log(hasedPassword);
     res.sendStatus(200)
+
+    //Create a new user and save to db
+    //calling the users model
+    //useing the create
+    const newUser = await prisma.user.create({
+        data:{
+            username,
+            email,
+            password:hasedPassword,
+        }
+    })
+    console.log(newUser);
 }
 
 export const login = (req, res) => {

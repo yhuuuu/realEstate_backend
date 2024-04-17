7942fc61c60777a70b79c018837c1e946b15dfb9
Yuting Hu <yuting.hu.careers@gmail.com>
2024-04-17 15:48:47 -0700

    log out function completed

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -85,5 +85,5 @@ export const login = async (req, res) => {
 }
 
 export const logout = (req, res) => {
-    //db operations
+    res.clearCookie("token").status(200).json({ message: "Logout Successfully!" })
 }

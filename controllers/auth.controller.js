84b63347c1c5c9e191b9f2c81e95b49ef4e90a60
Yuting Hu <yuting.hu.careers@gmail.com>
2024-04-16 23:32:06 -0700

    completed register endpoint

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,23 +4,27 @@ import prisma from '../lib/prisma.js';
 export const register = async (req, res) => {
 
     const { username, email, password } = req.body
+    try {
+        //HASH the Password
+        const hasedPassword = await bcrypt.hash(password, 10)
+        console.log(hasedPassword);
+        //Create a new user and save to db
+        //calling the users model
+        //useing the create
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hasedPassword,
+            }
+        })
+        console.log(newUser);
 
-    //HASH the Password
-    const hasedPassword = await bcrypt.hash(password, 10)
-    console.log(hasedPassword);
-    res.sendStatus(200)
-
-    //Create a new user and save to db
-    //calling the users model
-    //useing the create
-    const newUser = await prisma.user.create({
-        data:{
-            username,
-            email,
-            password:hasedPassword,
-        }
-    })
-    console.log(newUser);
+        res.status(201).json({ message: "User created successfully" })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Failed to create user!" })
+    }
 }
 
 export const login = (req, res) => {
